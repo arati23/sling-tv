d1deed4ed81f905dbc017991f086bab9658624df
arati23 <ureka.arti@gmail.com>
2019-01-12 17:13:24 -0700

    my first branch code commit

--- core/src/main/java/com/sling/tv/core/servlet/MatchInfoServlet.java
+++ core/src/main/java/com/sling/tv/core/servlet/MatchInfoServlet.java
@@ -29,7 +29,7 @@ public class MatchInfoServlet extends SlingSafeMethodsServlet {
 	
 
     /**
-     * comment statements are missing.hello Arati
+     * comment statements are missing.hello Arati jena
      */
     private static final long serialVersionUID = 1L;
     private final Logger log = LoggerFactory.getLogger(this.getClass());
